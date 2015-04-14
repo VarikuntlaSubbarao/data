03283f4056ea6c4ea0cef5de71f1c2a8c9a38640
androidvsrao <varikuntlasubbu@gmail.com>
2015-04-14 13:13:27 +0530

    first commit

---
+++ ServerRequests.java
@@ -0,0 +1,47 @@
+package info.nihar.services;
+
+public class ServerRequests {
+
+	public static final String HOME = "http://www.smartmelody.in/json";
+
+	public static final String NEW_ALBUMS = "http://www.raaga.com/json/newreleases.asp?l=";
+
+	public static final String MOSTPOPULAR_ALBUMS = "http://www.raaga.com/json/popular.asp?l=";
+
+	public static final String TOPTENLIST = "http://www.raaga.com/json/top10.asp?l=";
+
+	public static final String PLAYLIST = "http://www.raaga.com/json/playlist-v2.asp?id=";// ADD
+																							// SONG
+																							// ID
+																							// FOR
+																							// SONG
+																							// DETILS
+
+	public static final String TRACK_LIST = "http://www.raaga.com/json/Album.asp?aId=";// ADD
+																						// ALBUM
+																						// ID
+																						// FOR
+																						// SONG
+																						// LIST
+
+	public static final String SEARCH = "http://www.raaga.com/json/search-v2.asp?l=A&q=telugu&c=150";
+
+	public static final String BASED_ON_USER_ALBUMS = "http://www.raaga.com/json/mylib.asp?ip=";
+
+	public static final String RECENT_PLAY = "http://www.raaga.com/json/recentplays.asp?ip=<userId>";
+
+	public static final String USER_PLAT_LIST = "http://www.raaga.com/json/userPList.asp?ip=";
+
+	public static final String DEVOTIONAL = "http://www.raaga.com/json/just_in.asp?l=";
+
+	public static final String DEVOTIONAL_LINK = "&t=D";
+
+	public static final String COMPILATIONS_LINK = "&t=C";
+
+	public static final String TELUGU = "A";
+
+	public static final String TAMIL = "T";
+
+	public static final String HINDI = "H";
+
+}
